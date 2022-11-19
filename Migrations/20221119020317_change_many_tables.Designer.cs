7d8232cf81b86aff4c9d0fdd54996c06e80ba842
Bengeta <inovozhilov08@gmail.com>
2022-11-19 12:07:34 +1000

    change tables

--- Migrations/20221119020317_change_many_tables.Designer.cs
+++ Migrations/20221119020317_change_many_tables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VTBlockBackend.Data;
@@ -11,9 +12,11 @@ using VTBlockBackend.Data;
 namespace VTBlockBackend.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20221119020317_change_many_tables")]
+    partial class changemanytables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20221119020317_change_many_tables.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VTBlockBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class changemanytables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Check");
+
+            migrationBuilder.DropTable(
+                name: "MarketItemImage");
+
+            migrationBuilder.DropTable(
+                name: "MarketItemTag");
+
+            migrationBuilder.DropTable(
+                name: "TaskImage");
+
+            migrationBuilder.DropTable(
+                name: "TaskTag");
+
+            migrationBuilder.DropTable(
+                name: "UserMarketItem");
+
+            migrationBuilder.DropTable(
+                name: "UserTask");
+
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.DropTable(
+                name: "MarketItem");
+
+            migrationBuilder.DropTable(
+                name: "Task");
+
+            migrationBuilder.CreateTable(
+                name: "TransactionHistory",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Amount = table.Column<double>(type: "double precision", nullable: false),
+                    WalletFrom = table.Column<int>(type: "integer", nullable: false),
+                    WalletTo = table.Column<int>(type: "integer", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionHistory", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wallet",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Balance = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wallet", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Wallet_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wallet_UserId",
+                table: "Wallet",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransactionHistory");
+
+            migrationBuilder.DropTable(
+                name: "Wallet");
+
+            migrationBuilder.CreateTable(
+                name: "Check",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<double>(type: "double precision", nullable: false),
+                    CheckHash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Check", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Check_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MarketItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Category = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MarketItem", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedBy = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DeadLine = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Reward = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MarketItemImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ImageId = table.Column<int>(type: "integer", nullable: false),
+                    MarketItemId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MarketItemImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MarketItemImage_ImageStorage_MarketItemId",
+                        column: x => x.MarketItemId,
+                        principalTable: "ImageStorage",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MarketItemImage_MarketItem_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "MarketItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserMarketItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MarketItemId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMarketItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserMarketItem_MarketItem_MarketItemId",
+                        column: x => x.MarketItemId,
+                        principalTable: "MarketItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserMarketItem_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MarketItemTag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MarketItemId = table.Column<int>(type: "integer", nullable: false),
+                    TagId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MarketItemTag", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MarketItemTag_MarketItem_MarketItemId",
+                        column: x => x.MarketItemId,
+                        principalTable: "MarketItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MarketItemTag_Tag_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tag",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ImageId = table.Column<int>(type: "integer", nullable: false),
+                    TaskId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaskImage_ImageStorage_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "ImageStorage",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaskImage_Task_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Task",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskTag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TagId = table.Column<int>(type: "integer", nullable: false),
+                    TaskId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskTag", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaskTag_Tag_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tag",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaskTag_Task_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Task",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTask",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TaskId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTask", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserTask_Task_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Task",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserTask_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Check_CheckHash",
+                table: "Check",
+                column: "CheckHash",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Check_UserId",
+                table: "Check",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MarketItemImage_ImageId",
+                table: "MarketItemImage",
+                column: "ImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MarketItemImage_MarketItemId",
+                table: "MarketItemImage",
+                column: "MarketItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MarketItemTag_MarketItemId",
+                table: "MarketItemTag",
+                column: "MarketItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MarketItemTag_TagId",
+                table: "MarketItemTag",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskImage_ImageId",
+                table: "TaskImage",
+                column: "ImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskImage_TaskId",
+                table: "TaskImage",
+                column: "TaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskTag_TagId",
+                table: "TaskTag",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskTag_TaskId",
+                table: "TaskTag",
+                column: "TaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMarketItem_MarketItemId",
+                table: "UserMarketItem",
+                column: "MarketItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMarketItem_UserId",
+                table: "UserMarketItem",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTask_TaskId",
+                table: "UserTask",
+                column: "TaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTask_UserId",
+                table: "UserTask",
+                column: "UserId");
+        }
+    }
+}
